2c3aefb6619657551fcd872b0ff37facb84b05f2
amsboy <farahaneamossou@gmail.com>
2025-12-26 15:24:42 +0100

    Mise à jour config.php pour Render

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
-$host = getenv('DB_HOST');     // hôte de la base (ex: sql.freesqldatabase.com)
-$user = getenv('DB_USER');     // identifiant MySQL
-$pass = getenv('DB_PASS');     // mot de passe MySQL
-$db   = getenv('DB_NAME');     // nom de la base
+$host = "sql.freesqldatabase.com";
+$user = "sql5812991";   // ton identifiant
+$pass = "l1KAg2Wplt"; 
+$db   = "sql5812991";   // nom de ta base
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Vérification de la connexion
 if ($conn->connect_error) {
-    die("Erreur de connexion à la base : " . $conn->connect_error);
+    die("Connexion échouée: " . $conn->connect_error);
 }
 ?>
